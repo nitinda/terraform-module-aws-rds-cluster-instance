32782b85c8e4588681405c5e3e38639c9f1024da
nitin-das <nitin.das@thomson.co.uk>
2020-02-14 20:33:10 +0000

    Modified code

--- rds-cluster-instance.tf
+++ rds-cluster-instance.tf
@@ -1,6 +1,5 @@
 resource "aws_rds_cluster_instance" "rds_cluster_instance" {
-  count                           = 2
-  identifier                      = "${var.identifier}-${count.index}"
+  identifier                      = var.identifier
   cluster_identifier              = var.cluster_identifier
   instance_class                  = var.instance_class
   engine                          = var.engine
